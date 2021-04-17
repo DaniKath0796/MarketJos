cf15970e1c13557f2827de85c521d2c08eac97c3
Daniela <kath.ccama@gmail.com>
2021-04-16 22:14:24 -0500

    BD

--- inkatreat/next.config.js
+++ inkatreat/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        "BASE_URL": "http://localhost:3000",
+        "BASE_URL": "https://market-wine.vercel.app/",
         "MONGODB_URL": "mongodb+srv://kenny:k1e2n3n4y5@cluster0.5mboh.mongodb.net/inkatreat?retryWrites=true&w=majority",
         "ACCESS_TOKEN_SECRET": "{pH(X?N%PPes(>8FvCf}!7NM^r594an6UF$XzS7Q<`s<#-",
         "REFRESH_TOKEN_SECRET": "UF#RHG9k+h)8jQFB.9Htr'B+q~.[z9tDpz}5vPG}HgyvvPc2A9Q2cxgJ^}j@%C-7Tg/U2}x"
